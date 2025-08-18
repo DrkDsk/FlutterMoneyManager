bc707e17735e5f95de6e978e3467380f89579630
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-17 18:57:09 -0600

    feat: float button

--- lib/src/features/home/ui/pages/daily_home_page.dart
+++ lib/src/features/home/ui/pages/daily_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_money_manager/src/core/colors/app_colors.dart';
+import 'package:flutter_money_manager/src/features/home/ui/widgets/add_summary_button.dart';
 import 'package:flutter_money_manager/src/features/home/ui/widgets/calendar_page.dart';
 import 'package:flutter_money_manager/src/features/home/ui/widgets/daily_balance_widget.dart';
 import 'package:flutter_money_manager/src/features/home/ui/widgets/home_tab_bar.dart';
@@ -24,34 +25,43 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 14),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 60),
-          const SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: SummaryContent()
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 60),
+              const SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: SummaryContent()
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                    color: AppColors.secondary.withOpacity(0.60),
+                    borderRadius: BorderRadius.circular(12)),
+                child: HomeTabBar(tabController: _tabController),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: TabBarView(controller: _tabController, children: const [
+                  DailyBalancePage(),
+                  CalendarPage(),
+                  DailyBalancePage(),
+                ]),
+              )
+            ],
           ),
-          const SizedBox(height: 20),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-                color: AppColors.secondary.withOpacity(0.60),
-                borderRadius: BorderRadius.circular(12)),
-            child: HomeTabBar(tabController: _tabController),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: TabBarView(controller: _tabController, children: const [
-              DailyBalancePage(),
-              CalendarPage(),
-              DailyBalancePage(),
-            ]),
-          )
-        ],
-      ),
+        ),
+        const Positioned(
+          bottom: 30,
+          right: 30,
+          child: AddSummaryButton()
+        )
+      ],
     );
   }
 }
--- lib/src/features/home/ui/widgets/daily_balance_widget.dart
+++ lib/src/features/home/ui/widgets/daily_balance_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_money_manager/src/features/home/ui/widgets/add_summary_button.dart';
 import 'package:flutter_money_manager/src/features/home/ui/widgets/expense_list.dart';
 
 class DailyBalancePage extends StatelessWidget {
@@ -11,34 +10,25 @@ class DailyBalancePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Stack(
-      children: [
-        SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 10),
-              Text(
-                "Transaction on August",
-                style: TextStyle(
-                  color: theme.colorScheme.onPrimary,
-                  fontSize: 24,
-                ),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.90,
-                child: const ExpenseList(),
-              ),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            "Transaction on August",
+            style: TextStyle(
+              color: theme.colorScheme.onPrimary,
+              fontSize: 24,
+            ),
           ),
-        ),
-        const Positioned(
-          bottom: 40,
-          right: 0,
-          child: AddSummaryButton(),
-        ),
-      ],
+          const SizedBox(height: 10),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.90,
+            child: const ExpenseList(),
+          ),
+        ],
+      ),
     );
   }
 }
