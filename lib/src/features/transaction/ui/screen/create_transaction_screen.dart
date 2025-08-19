5c9cb94bbc24e83242bfb0c41a4b0f3f4983c00e
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-18 23:03:19 -0600

    feat: custom focus

--- lib/src/features/transaction/ui/screen/create_transaction_screen.dart
+++ lib/src/features/transaction/ui/screen/create_transaction_screen.dart
@@ -7,33 +7,74 @@ class CreateTransactionScreen extends StatefulWidget {
   const CreateTransactionScreen({super.key});
 
   @override
-  State<CreateTransactionScreen> createState() => _CreateTransactionScreenState();
+  State<CreateTransactionScreen> createState() =>
+      _CreateTransactionScreenState();
 }
 
 class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
+  String defaultAmountValue = "\$ 0.00";
 
-  final FocusNode _focusNode = FocusNode();
+  late FocusNode _focusNode;
   late TextEditingController _noteController;
+  late TextEditingController _amountController;
 
   @override
   void initState() {
     _noteController = TextEditingController();
+    _amountController = TextEditingController(text: defaultAmountValue);
+    _focusNode = FocusNode();
     super.initState();
   }
 
   @override
   void dispose() {
+    _noteController.dispose();
+    _amountController.dispose();
     _focusNode.dispose();
     super.dispose();
+  }
+
+  void _formatAmountOnBlur() {
+    _focusNode.unfocus();
+
+    if (_amountController.text == "0") {
+      _amountController.clear();
+      _amountController.text = defaultAmountValue;
+      return ;
+    }
+
+    final onlyAmount = _amountController.text.replaceAll("\$ ", "").replaceAll(" ", "");
+    _amountController.text = "\$ $onlyAmount";
+  }
+
+  void _sanitizeInput(String value) {
+    if (value.isEmpty || value == "0" || value == "00") {
+      _amountController.text = "0";
+      return;
+    }
+
+    var cleaned = value.replaceAll("\$", "");
+
+    if (cleaned.startsWith("0")) {
+      cleaned = cleaned.replaceFirst("0", "");
+    }
+
+    _amountController.text = cleaned;
+  }
+
+  void _resetIfDefault() {
+    if (_amountController.text == defaultAmountValue) {
+      _amountController.text = "0";
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    final largeStyle = theme.textTheme.bodyLarge
+    final mediumStyle = theme.textTheme.bodyLarge
         ?.copyWith(fontSize: 20, fontWeight: FontWeight.w400);
-    final smallStyle = theme.textTheme.bodyMedium?.copyWith(fontSize: 20);
+    final largeStyle = theme.textTheme.bodyMedium?.copyWith(fontSize: 20);
 
     return Scaffold(
       appBar: const CustomAppBar(),
@@ -43,10 +84,12 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                  color: AppColors.turquoise,
-                  borderRadius: BorderRadius.circular(12)
-                ),
-                child: Center(child: Text("Save", style: theme.textTheme.bodyMedium?.copyWith(color: theme.colorScheme.primary))),
+                    color: AppColors.turquoise,
+                    borderRadius: BorderRadius.circular(12)),
+                child: Center(
+                    child: Text("Save",
+                        style: theme.textTheme.bodyMedium
+                            ?.copyWith(color: theme.colorScheme.primary))),
               ),
             ),
             const SizedBox(width: 10),
@@ -54,9 +97,11 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
               padding: const EdgeInsets.symmetric(horizontal: 24),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: theme.colorScheme.secondary)
-              ),
-              child: Center(child: Text("Continue", style: theme.textTheme.bodyMedium?.copyWith(color: theme.colorScheme.secondary))),
+                  border: Border.all(color: theme.colorScheme.secondary)),
+              child: Center(
+                  child: Text("Continue",
+                      style: theme.textTheme.bodyMedium
+                          ?.copyWith(color: theme.colorScheme.secondary))),
             )
           ],
         ),
@@ -64,7 +109,9 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Container(
               margin: const EdgeInsets.all(10),
               padding: const EdgeInsets.symmetric(vertical: 10),
@@ -77,7 +124,7 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text("Date", style: smallStyle),
+                        Text("Date", style: largeStyle),
                         GestureDetector(
                             onTap: () {
                               final today = DateTime.now();
@@ -91,7 +138,40 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
                                   firstDate: firstDate,
                                   lastDate: lastDate);
                             },
-                            child: Text("Aug 18, 2025", style: largeStyle))
+                            child: Text("Aug 18, 2025", style: mediumStyle))
+                      ],
+                    ),
+                  ),
+                  const Divider(height: 20, color: Colors.grey, thickness: 0.2),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: TextFormField(
+                      textAlign: TextAlign.right,
+                      controller: _amountController,
+                      focusNode: _focusNode,
+                      style: mediumStyle?.copyWith(
+                          color: AppColors.expenseColor,
+                          fontWeight: FontWeight.w700),
+                      onTapOutside: (_) => _formatAmountOnBlur(),
+                      onChanged: _sanitizeInput,
+                      onTap: _resetIfDefault,
+                      keyboardType: TextInputType.number,
+                      enabled: true,
+                      decoration: InputDecoration(
+                        prefixText: "Amount",
+                        prefixStyle: mediumStyle,
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const Divider(height: 20, color: Colors.grey, thickness: 0.2),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Category", style: largeStyle),
+                        Text("Uncategorized", style: mediumStyle)
                       ],
                     ),
                   ),
@@ -101,32 +181,8 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text("Amount", style: smallStyle),
-                        Text("\$ 0.00",
-                            style:
-                                largeStyle?.copyWith(color: AppColors.expenseColor))
-                      ],
-                    ),
-                  ),
-                  const Divider(height: 20, color: Colors.grey, thickness: 0.2),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Category", style: smallStyle),
-                        Text("Uncategorized", style: largeStyle)
-                      ],
-                    ),
-                  ),
-                  const Divider(height: 20, color: Colors.grey, thickness: 0.2),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Payment Source", style: smallStyle),
-                        Text("None", style: largeStyle)
+                        Text("Payment Source", style: largeStyle),
+                        Text("None", style: mediumStyle)
                       ],
                     ),
                   ),
@@ -139,18 +195,14 @@ class _CreateTransactionScreenState extends State<CreateTransactionScreen> {
               margin: const EdgeInsets.symmetric(horizontal: 10),
               child: TextFormField(
                 controller: _noteController,
-                autofocus: true,
-                focusNode: _focusNode,
                 style: theme.textTheme.bodySmall,
                 decoration: InputDecoration(
-                  hintText: "Enter a note",
-                  hintStyle: TextStyle(
-                    color: AppColors.onPrimary.withOpacity(0.5)
-                  ),
+                    hintText: "Enter a note",
+                    hintStyle:
+                        TextStyle(color: AppColors.onPrimary.withOpacity(0.5)),
                     filled: true,
-                  fillColor: Colors.grey.withOpacity(0.05),
-                  border: InputBorder.none
-                ),
+                    fillColor: Colors.grey.withOpacity(0.05),
+                    border: InputBorder.none),
               ),
             )
           ],
