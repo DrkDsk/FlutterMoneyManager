fc74f24610dc2af9f25bebd531e136e3a4747d28
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-21 11:32:01 -0600

    feat: widgets with rounded defined

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_money_manager/src/core/di/di.dart';
-import 'package:flutter_money_manager/src/core/theme/theme__helper.dart';
+import 'package:flutter_money_manager/src/core/shared/theme/theme__helper.dart';
 import 'package:flutter_money_manager/src/features/home/ui/blocs/home_redirection_cubit.dart';
 import 'package:flutter_money_manager/src/features/home/ui/blocs/navigation_cubit.dart';
 import 'package:flutter_money_manager/src/features/splash/ui/screen/welcome_screen.dart';
