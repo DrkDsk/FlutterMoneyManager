b7a919f15a3e359fcfa31edbf20f6246813d1a3e
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-25 14:42:01 -0600

    feat: cstate modified entity

--- lib/src/features/transaction/ui/blocs/cubit/create_transaction_cubit.dart
+++ lib/src/features/transaction/ui/blocs/cubit/create_transaction_cubit.dart
@@ -6,33 +6,45 @@ import 'package:flutter_money_manager/src/features/transaction/domain/entities/t
 import 'package:flutter_money_manager/src/features/transaction/ui/blocs/cubit/create_transaction_state.dart';
 
 class CreateTransactionCubit extends Cubit<CreateTransactionState> {
-  CreateTransactionCubit()
-      : super(CreateTransactionState(transactionDate: DateTime.now()));
+  CreateTransactionCubit() : super(CreateTransactionState());
 
   void updateAmountDate(DateTime? time) {
-    final newState = state.copyWith(transactionDate: time);
+    final transaction = state.transactionEntity.copyWith(transactionDate: time);
+    final newState = state.copyWith(transaction: transaction);
+
     emit(_validForm(newState));
   }
 
   void updateAmount(String? amount) {
-    final newState = state.copyWith(amount: amount);
+    final transaction = state.transactionEntity.copyWith(amount: amount);
+    final newState =
+        state.copyWith(transaction: transaction.copyWith(amount: amount));
+
     emit(_validForm(newState));
   }
 
   void updateTransactionSource(TransactionSource transactionSource) {
-    final newState = state.copyWith(transactionSource: transactionSource);
+    final transaction = state.transactionEntity;
+    final newState = state.copyWith(
+        transaction: transaction.copyWith(source: transactionSource));
+
     emit(_validForm(newState));
   }
 
   void updateTransactionCategory(TransactionCategory category) {
-    final newState = state.copyWith(transactionCategory: category);
+    final transaction = state.transactionEntity;
+    final newState =
+        state.copyWith(transaction: transaction.copyWith(category: category));
+
     emit(_validForm(newState));
   }
 
   CreateTransactionState _validForm(CreateTransactionState newState) {
-    final amount = newState.amount.replaceAll("\$ ", "").replaceAll(" ", "");
-    final transactionSource = newState.transactionSource;
-    final transactionCategory = newState.transactionCategory;
+    final transaction = newState.transactionEntity;
+
+    final amount = transaction.amount.replaceAll("\$ ", "").replaceAll(" ", "");
+    final transactionSource = transaction.source;
+    final transactionCategory = transaction.category;
 
     final bool formIsValidated = (transactionSource == null ||
             transactionCategory == null ||
@@ -45,8 +57,11 @@ class CreateTransactionCubit extends Cubit<CreateTransactionState> {
   }
 
   void updateTransactionType(int index) {
-    final transaction = kDefaultTransactionTypes[index];
+    final transaction = state.transactionEntity;
 
-    emit(state.copyWith(transactionType: transaction.type));
+    final transactionSelected = kDefaultTransactionTypes[index];
+
+    emit(state.copyWith(
+        transaction: transaction.copyWith(type: transactionSelected.type)));
   }
 }
--- lib/src/features/transaction/ui/blocs/cubit/create_transaction_state.dart
+++ lib/src/features/transaction/ui/blocs/cubit/create_transaction_state.dart
@@ -1,48 +1,27 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_money_manager/src/core/constants/transactions_constants.dart';
 import 'package:flutter_money_manager/src/core/enums/transaction_type.dart';
-import 'package:flutter_money_manager/src/features/transaction/domain/entities/transaction_source.dart';
-import 'package:flutter_money_manager/src/features/transaction/domain/entities/transaction_category.dart';
+import 'package:flutter_money_manager/src/features/transaction/domain/entities/transaction_entity.dart';
 
 class CreateTransactionState with EquatableMixin {
-  final DateTime transactionDate;
-  final String amount;
-  final TransactionSource? transactionSource;
-  final TransactionCategory? transactionCategory;
+  final TransactionEntity transactionEntity;
   final bool formIsValidated;
-  final TransactionType transactionType;
 
   CreateTransactionState(
-      {required this.transactionDate,
-      this.amount = kDefaultAmountValue,
-      this.transactionSource,
-      this.transactionCategory,
-      this.formIsValidated = false,
-      this.transactionType = TransactionType.income});
+      {TransactionEntity? transactionEntity, this.formIsValidated = false})
+      : transactionEntity = transactionEntity ??
+            TransactionEntity(
+              amount: kDefaultAmountValue,
+              type: TransactionType.income,
+            );
 
   CreateTransactionState copyWith(
-      {DateTime? transactionDate,
-      String? amount,
-      TransactionSource? transactionSource,
-      TransactionCategory? transactionCategory,
-      bool? formIsValidated,
-      TransactionType? transactionType}) {
+      {TransactionEntity? transaction, bool? formIsValidated}) {
     return CreateTransactionState(
-        transactionDate: transactionDate ?? this.transactionDate,
-        amount: amount ?? this.amount,
-        transactionSource: transactionSource ?? this.transactionSource,
-        transactionCategory: transactionCategory ?? this.transactionCategory,
-        formIsValidated: formIsValidated ?? this.formIsValidated,
-        transactionType: transactionType ?? this.transactionType);
+        transactionEntity: transaction ?? transactionEntity,
+        formIsValidated: formIsValidated ?? this.formIsValidated);
   }
 
   @override
-  List<Object?> get props => [
-        transactionDate,
-        amount,
-        transactionSource,
-        transactionCategory,
-        formIsValidated,
-        transactionType
-      ];
+  List<Object?> get props => [transactionEntity, formIsValidated];
 }
