98a77330ce2ca7724ce8d861d1d11217a6f42b4f
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-25 21:21:49 -0600

    feat: repository impl

---
+++ lib/src/features/transaction/domain/repositories/transaction_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_money_manager/src/core/error/failure/failure.dart';
+import 'package:flutter_money_manager/src/features/transaction/domain/entities/transaction.dart';
+
+abstract interface class TransactionRepository {
+  Future<Either<Failure, bool>> saveTransaction(Transaction transaction);
+}
