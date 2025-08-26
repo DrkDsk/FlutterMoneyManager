6005e77a1ca2cdc70025e0b8586b18b9de3e5592
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-25 21:21:23 -0600

    feat: line removed

--- lib/src/core/helpers/hive_initializer.dart
+++ lib/src/core/helpers/hive_initializer.dart
@@ -1,13 +1,10 @@
-import 'package:flutter_money_manager/src/core/constants/hive_constants.dart';
 import 'package:flutter_money_manager/src/features/transaction/data/models/transaction_hive_model.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveInitializer {
-  static Future<Box<TransactionHiveModel>> init() async {
+  static Future<void> init() async {
     await Hive.initFlutter();
 
     Hive.registerAdapter(TransactionHiveModelAdapter());
-
-    return await Hive.openBox<TransactionHiveModel>(hiveTransactionBoxName);
   }
 }
