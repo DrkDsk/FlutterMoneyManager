25aa07834a3fc351bcb1ecdfa13dfe5ffb66e9e1
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-25 21:20:41 -0600

    feat: call to init hive

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ import 'package:flutter_money_manager/src/core/helpers/hive_initializer.dart';
 import 'package:flutter_money_manager/src/my_app.dart';
 
 void main() async {
-  await initDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
   await HiveInitializer.init();
+  await initDependencies();
 
   runApp(const MyApp());
 }
