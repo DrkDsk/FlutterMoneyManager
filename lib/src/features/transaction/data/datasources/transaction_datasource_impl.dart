edc08c46765bf82e403409c2ed1f63f3f1ab0d10
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-25 20:05:17 -0600

    feat: datasource implemented

---
+++ lib/src/features/transaction/data/datasources/transaction_datasource_impl.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_money_manager/src/features/transaction/data/datasources/transaction_datasource.dart';
+import 'package:flutter_money_manager/src/features/transaction/data/models/transaction_hive_model.dart';
+import 'package:flutter_money_manager/src/features/transaction/domain/entities/transaction.dart';
+import 'package:hive/hive.dart';
+
+class TransactionDatasourceImpl implements TransactionDatasource {
+  final Box<TransactionHiveModel> box;
+
+  const TransactionDatasourceImpl({required this.box});
+
+  @override
+  Future<bool> saveTransaction(Transaction transaction) async {
+    final hiveModel = TransactionHiveModel.fromEntity(transaction);
+
+    await box.add(hiveModel);
+    return true;
+  }
+}
