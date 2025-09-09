608a5090f13e6b0769bae12d2383ee3c938c1ca4
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-09-09 15:13:28 -0600

    feat: const added to define box name

--- lib/src/core/constants/hive_constants.dart
+++ lib/src/core/constants/hive_constants.dart
@@ -2,3 +2,4 @@ const String hiveTransactionBoxName = "transactions";
 const String hiveTransactionSourceBoxName = "transaction_sources";
 const String hiveGlobalBalanceBoxName = "global_balance";
 const String hiveYearBalanceBoxName = "year_by_balance";
+const String hiveYearTransactionsBoxName = "transactions_year";
